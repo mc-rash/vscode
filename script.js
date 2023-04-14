c209184b800e46094063e6260ceeff6915085a9f
muhammed irfan <irfannadapuram@gmail.com>
2023-04-15 00:27:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggle(){
+    var menu = document.getElementById("items");
+    if (menu.style.display==="flex") {
+        menu.style.display="none";
+    }
+    else {
+        menu.style.display ="flex"
+    }
+}
